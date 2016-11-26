654a434b670e8067a8fabb157beb60e3aea13ef6
Sinan Goo <sinan.goo@hypoport.de>
2016-11-26 20:19:48 +0100

    Add option to choose ssl or not

--- run.sh
+++ run.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 set -ef -o pipefail
 
-while getopts "d:p:r:vs" OPTIONS; do case $OPTIONS in
+while getopts "p:vs" OPTIONS; do case $OPTIONS in
   v) VERBOSE_OPTIONS="-vv" ;;
   p) PORT="$OPTARG" ;;
-  *) exit 1 ;;
+  s) ssl="1" ;;
 esac; done; shift $(( OPTIND - 1 ))
 
 : ${PORT:="8080"}
 : ${VERBOSE_OPTIONS:=""}
 : ${SERVICE:="$(dirname $0)/service.sh"}
 
+if [[ "$ssl" -eq 1 ]];then
+  socat_listen_command="OPENSSL-LISTEN:${PORT},reuseaddr,fork,verify=0,cert=ssl_test_certificates/server.pem"
+else
+  socat_listen_command="TCP-LISTEN:${PORT},reuseaddr,fork"
+fi
+
 socat \
   $VERBOSE_OPTIONS \
-  OPENSSL-LISTEN:${PORT},reuseaddr,fork,verify=0,cert=ssl_test_certificates/server.pem \
+  $socat_listen_command \
   EXEC:"${SERVICE}"
 
