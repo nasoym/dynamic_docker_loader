f7f0f07dc8ce748f8db1a2441e02e4cc6d8d9af4
sinan <sinangoo@gmx.de>
2016-12-07 21:11:18 +0100

    fix override parameter for insecure setup without ssl

--- run.sh
+++ run.sh
@@ -4,14 +4,14 @@ set -ef -o pipefail
 while getopts "p:vs" OPTIONS; do case $OPTIONS in
   v) VERBOSE_OPTIONS="-vv" ;;
   p) PORT="$OPTARG" ;;
-  i) ssl="0" ;;
+  i) insecure=1 ;;
 esac; done; shift $(( OPTIND - 1 ))
 
 : ${PORT:="8080"}
 : ${VERBOSE_OPTIONS:=""}
 : ${SERVICE:="$(dirname $0)/service.sh"}
 
-if [[ "$ssl" -eq 0 ]];then
+if [[ "$insecure" -eq 1 ]];then
   socat_listen_command="TCP-LISTEN:${PORT},reuseaddr,fork"
 else
   socat_listen_command="OPENSSL-LISTEN:${PORT},reuseaddr,fork,verify=0,cert=certificate/server.pem"
