95acc05c4b50b7d4b5efac7de38d6f0b68224380
sinan <sinangoo@gmx.de>
2016-12-08 20:19:46 +0100

    execute request to docker container

--- service.sh
+++ service.sh
@@ -64,29 +64,29 @@ echo "whoami:$(whoami)" >&2
 # echo "authorization_jwt:${AUTHORIZATION_JWT_TOKEN}" >&2
 # echo "authorization_type:${AUTHORIZATION_TYPE}<" >&2
 
-# docker_ports="$(docker ps -f status=running -f ancestor=${docker_image} --format "{{.Ports}}")"
-# docker_image_id="$(docker ps -f status=running -f ancestor=${docker_image} --format "{{.Image}}")"
-# docker_image_created="$(docker inspect ${docker_image} | jq -r '.[0].Created')"
-#
-# if [[ -z "$docker_ports" ]] && [[ -n "$docker_image" ]];then
-#   echo "run docker_image:$docker_image" >&2
-#   docker run -d -P $docker_image >&2
-#   docker_ports="$(docker ps -f status=running -f ancestor=${docker_image} --format "{{.Ports}}")"
-# fi
-# if [[ -n "$docker_ports" ]];then
-#   public_port="$(echo "$docker_ports" | awk -F',' '{print $1}' | sed 's/^.*:\([0-9]*\)->.*$/\1/g')"
-#   if [[ -n "$public_port" ]];then
-#     response="$( \
-#     echo -n "${REQUEST_METHOD} ${pass_on_uri} ${REQUEST_HTTP_VERSION}
-# ${ALL_LINES}${REQUEST_CONTENT}" \
-#     | socat - TCP:localhost:${public_port} \
-#     )"
-#     sed -n '1p' <<<"${response}"
-#     echo "Docker_Image_Created: ${docker_image_created}"
-#     sed -n '2,$p' <<<"${response}"
-#     exit 0
-#   fi
-# fi
+docker_ports="$(docker ps -f status=running -f ancestor=${docker_image} --format "{{.Ports}}")"
+docker_image_id="$(docker ps -f status=running -f ancestor=${docker_image} --format "{{.Image}}")"
+docker_image_created="$(docker inspect ${docker_image} | jq -r '.[0].Created')"
+
+if [[ -z "$docker_ports" ]] && [[ -n "$docker_image" ]];then
+  echo "run docker_image:$docker_image" >&2
+  docker run -d -P $docker_image >&2
+  docker_ports="$(docker ps -f status=running -f ancestor=${docker_image} --format "{{.Ports}}")"
+fi
+if [[ -n "$docker_ports" ]];then
+  public_port="$(echo "$docker_ports" | awk -F',' '{print $1}' | sed 's/^.*:\([0-9]*\)->.*$/\1/g')"
+  if [[ -n "$public_port" ]];then
+    response="$( \
+    echo -n "${REQUEST_METHOD} ${pass_on_uri} ${REQUEST_HTTP_VERSION}
+${ALL_LINES}${REQUEST_CONTENT}" \
+    | socat - TCP:localhost:${public_port},shut-none \
+    )"
+    sed -n '1p' <<<"${response}"
+    echo "Docker_Image_Created: ${docker_image_created}"
+    sed -n '2,$p' <<<"${response}"
+    exit 0
+  fi
+fi
 
 STATUS_CODE=${1-404}
 STATUS_TEXT=${2-"Not Found"}
