67d573896ec0af63700c3a54a795ba30ac050344
sinan <sinangoo@gmx.de>
2016-12-08 23:12:43 +0100

    put server certificate file name into variable

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ef -o pipefail
 
-while getopts "p:vs" OPTIONS; do case $OPTIONS in
+while getopts "p:vi" options; do case $options in
   v) VERBOSE_OPTIONS="-vv" ;;
   p) PORT="$OPTARG" ;;
   i) insecure=1 ;;
@@ -10,11 +10,12 @@ esac; done; shift $(( OPTIND - 1 ))
 : ${PORT:="8080"}
 : ${VERBOSE_OPTIONS:=""}
 : ${SERVICE:="$(dirname $0)/service.sh"}
+: ${SERVER_PEM_FILE:="certificate/server.pem"}
 
 if [[ "$insecure" -eq 1 ]];then
   socat_listen_command="TCP-LISTEN:${PORT},reuseaddr,fork"
 else
-  socat_listen_command="OPENSSL-LISTEN:${PORT},reuseaddr,fork,verify=0,cert=certificate/server.pem"
+  socat_listen_command="OPENSSL-LISTEN:${PORT},reuseaddr,fork,verify=0,cert=${SERVER_PEM_FILE}"
 fi
 
 socat \
